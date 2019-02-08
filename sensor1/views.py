851481408c33e95e0e23346aaa6fafdd91d1944d
loryu <chinaloryu@gmail.com>
2019-02-08 15:52:40 +0800

    Update views.py
    @20190208 new

--- sensor1/views.py
+++ sensor1/views.py
@@ -4,4 +4,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Test for git.</br>'+'Test for release.')
+    return HttpResponse('Test for git.</br>'+'Test for release new.')
